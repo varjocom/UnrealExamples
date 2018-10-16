86ecaa4b8fff36ea08280141d0b97ac2b3d019bf
Markus Kivioja <markus.kivioja@varjo.com>
2018-10-16 12:03:53 +0300

    Fixed packaging.

--- VarjoSample/Source/VarjoSample/VarjoSample.Build.cs
+++ VarjoSample/Source/VarjoSample/VarjoSample.Build.cs
@@ -11,7 +11,7 @@ public class VarjoSample : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "VarjoHMD" });
         PublicIncludePathModuleNames.AddRange(new string[] { "VarjoHMD" });
 
-        PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PrivateDependencyModuleNames.AddRange(new string[] { "HeadMountedDisplay" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
