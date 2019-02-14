732922705b8daf56a1947b60a4e4cde536fb15db
Hakhyun Kim <hakhyunkim@outlook.com>
2019-02-14 19:06:35 +0200

    Clean asset (#5)
    * Removed unnecessary files
    
    * Update license
    
    * Move data to VarjoContext other than StarterContent
    
    * Remove unncessary startercontent
    
    * Move varjo assets tto varjocontest folder
    
    * Fix time formation

--- VarjoSample/Source/VarjoSample/VarjoSample.Build.cs
+++ VarjoSample/Source/VarjoSample/VarjoSample.Build.cs
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Varjo Technologies Oy. All Rights Reserved.
 
 using UnrealBuildTool;
 
@@ -7,18 +7,7 @@ public class VarjoSample : ModuleRules
 	public VarjoSample(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "VarjoHMD", "VarjoEyeTracker" });
-        PublicIncludePathModuleNames.AddRange(new string[] { "VarjoHMD" });
-
-        PrivateDependencyModuleNames.AddRange(new string[] { "HeadMountedDisplay" });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+        //PrivateDependencyModuleNames.AddRange(new string[] { "HeadMountedDisplay" });
 	}
 }
